857b3d9f5bab82a7fb21e1df18f54ccbbb1e2c67
careful-illustrator-5297 <anthonyroma741@gmail.com>
2020-08-25 15:46:33 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeForBirhdate < ActiveRecord::Migrations[5.1]
   def change
-    change_column(:students, :birthdate,:datetime )
+    change_column :students, :birthdate,:datetime 
   end
 end
